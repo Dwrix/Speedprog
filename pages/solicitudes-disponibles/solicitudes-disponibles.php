4e57307d5c3af326d1879dd143af9c168dd80031
tw1ZTER <al3x_18_139@hotmail.com>
2022-10-20 04:04:55 -0300

    arreglo de css y detalle-solicitud

--- pages/solicitudes-disponibles/solicitudes-disponibles.php
+++ pages/solicitudes-disponibles/solicitudes-disponibles.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 
-    <link rel="stylesheet" href="/css/solicitudes-disponibles.css">
+    <link rel="stylesheet" href="../../css/solicitudes-disponibles.css">
     <script src="/js/jquery-3.5.1.min.js"></script>
     <script src="/js/Slider.js"></script>
 
@@ -29,7 +29,48 @@
 
     <section>
 <div>
-    SOLICITUDES DISPONIBLES
+    <?php 
+    
+    $conexion = mysqli_connect("localhost:3306","root","root","speedprogasesorias") or die("Problemas con la conexion");
+    $sql = "SELECT * FROM solicitud WHERE estado_solicitud_fk = '1'";
+    $registros = mysqli_query($conexion, $sql) or die("Problemas en la seleccion:" . mysqli_error($conexion));
+    ?>
+    DETALLE SOLICITUD
+    
+    
+    <!-- <form action="../detalle-solicitud/detalle-solicitud.php"> -->
+    
+    
+    
+    <table border="1" width="700" align="center">
+    <tr>
+        <td style="display:none;">ID</td>
+        <td>Titulo</td>
+        <td>Especialidad</td>
+        <td>Descripcion</td>   
+        <td>Detalles</td>   
+    </tr>
+    <?php 
+    while ($reg = mysqli_fetch_array($registros)){
+        $dato = $reg['id_especialidad_fk'];
+        ?>
+    <tr>
+    <td style="display:none;"><?php echo $reg['id_solicitud'] ?></td>  
+    <td><?php echo $reg['titulo'] ?></td>
+    <?php 
+        $sql2 = "SELECT especialidad FROM especialidad WHERE id_especialidad = $dato";
+        $registros2 = mysqli_query($conexion, $sql2) or die("Problemas en la seleccion:" . mysqli_error($conexion));
+        $reg2 = mysqli_fetch_row($registros2);
+    ?>
+    <td><?php echo $reg2[0] ?></td>
+    <td><?php echo $reg['descripcion'] ?></td>
+    <td><a href="../detalle-solicitud/detalle-solicitud.php?=<?php echo $reg['id_solicitud'] ?>"> Ver detalles </td>
+    <?php }
+    mysqli_close($conexion);
+    ?>
+    </table>
+    
+    
 </div>
     </section>
     <footer>
@@ -38,38 +79,38 @@
                 <div class="colum2">
                     <h1>Redes Sociales</h1>
                     <div class="row">
-                        <a href="https://www.facebook.com/"><img src="/img/fbicon.png"></a>
+                        <a href="https://www.facebook.com/"><img src="../..//img/fbicon.png"></a>
                         <label>Siguenos en Facebook</label>
                     </div>
                     <div class="row">
-                        <a href="https://twitter.com/?lang=es"><img src="/img/twittericon.png"></a>
+                        <a href="https://twitter.com/?lang=es"><img src="../..//img/twittericon.png"></a>
                         <label>Siguenos en Twitter</label>
                     </div>
                     <div class="row">
-                        <a href="https://www.instagram.com/?hl=es-la"><img src="/img/igicon.png"></a>
+                        <a href="https://www.instagram.com/?hl=es-la"><img src="../..//img/igicon.png"></a>
                         <label>Siguenos en Instagram</label>
                     </div>
                     <div class="row">
-                        <a href="https://myaccount.google.com/?tab=kk"><img src="/img/gmailicon.png"></a>
+                        <a href="https://myaccount.google.com/?tab=kk"><img src="../..//img/gmailicon.png"></a>
                         <label>Siguenos en Google+</label>
                     </div>
                     <div class="row">
-                        <a href="https://www.pinterest.cl/"><img src="/img/pinteresticon.png"></a>
+                        <a href="https://www.pinterest.cl/"><img src="../..//img/pinteresticon.png"></a>
                         <label>Siguenos en Pinterest</label>
                     </div>
                 </div>
                 <div class="colum3">
                     <h1>Informacion Contactos</h1>
                     <div class="row2">
-                        <img src="/img/locationicon.png">
+                        <img src="../..//img/locationicon.png">
                         <label>Ahumada 312, oficina 108 entrepiso. Santiago Centro. Chile</label>
                     </div>
                     <div class="row2">
-                        <img src="/img/phone.png">
+                        <img src="../..//img/phone.png">
                         <label>(56)-2-2695-79-19</label>
                     </div>
                     <div class="row2">
-                        <img src="/img/mailicon.png">
+                        <img src="../..//img/mailicon.png">
                         <label>info@abogadosunited.cl</label>
                     </div>
                 </div>
