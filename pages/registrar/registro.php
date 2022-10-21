3b339a9c638071707341ae8c1b4bdee161c8614d
tw1ZTER <al3x_18_139@hotmail.com>
2022-10-21 16:26:15 -0300

    update

--- pages/registrar/registro.php
+++ pages/registrar/registro.php
@@ -6,8 +6,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 
     <link rel="stylesheet" href="../../css/registrar.css">
-    <script src="/js/jquery-3.5.1.min.js"></script>
-    <script src="/js/Slider.js"></script>
+    <script src="../../js/jquery-3.5.1.min.js"></script>
+    <script src="../../js/Slider.js"></script>
 
     <title>SpeedProg</title>
 
@@ -76,38 +76,38 @@
                 <div class="colum2">
                     <h1>Redes Sociales</h1>
                     <div class="row">
-                        <a href="https://www.facebook.com/"><img src="/img/fbicon.png"></a>
+                        <a href="https://www.facebook.com/"><img src="../../img/fbicon.png"></a>
                         <label>Siguenos en Facebook</label>
                     </div>
                     <div class="row">
-                        <a href="https://twitter.com/?lang=es"><img src="/img/twittericon.png"></a>
+                        <a href="https://twitter.com/?lang=es"><img src="../../img/twittericon.png"></a>
                         <label>Siguenos en Twitter</label>
                     </div>
                     <div class="row">
-                        <a href="https://www.instagram.com/?hl=es-la"><img src="/img/igicon.png"></a>
+                        <a href="https://www.instagram.com/?hl=es-la"><img src="../../img/igicon.png"></a>
                         <label>Siguenos en Instagram</label>
                     </div>
                     <div class="row">
-                        <a href="https://myaccount.google.com/?tab=kk"><img src="/img/gmailicon.png"></a>
+                        <a href="https://myaccount.google.com/?tab=kk"><img src="../../img/gmailicon.png"></a>
                         <label>Siguenos en Google+</label>
                     </div>
                     <div class="row">
-                        <a href="https://www.pinterest.cl/"><img src="/img/pinteresticon.png"></a>
+                        <a href="https://www.pinterest.cl/"><img src="../../img/pinteresticon.png"></a>
                         <label>Siguenos en Pinterest</label>
                     </div>
                 </div>
                 <div class="colum3">
                     <h1>Informacion Contactos</h1>
                     <div class="row2">
-                        <img src="/img/locationicon.png">
+                        <img src="../../img/locationicon.png">
                         <label>Ahumada 312, oficina 108 entrepiso. Santiago Centro. Chile</label>
                     </div>
                     <div class="row2">
-                        <img src="/img/phone.png">
+                        <img src="../../img/phone.png">
                         <label>(56)-2-2695-79-19</label>
                     </div>
                     <div class="row2">
-                        <img src="/img/mailicon.png">
+                        <img src="../../img/mailicon.png">
                         <label>info@abogadosunited.cl</label>
                     </div>
                 </div>
