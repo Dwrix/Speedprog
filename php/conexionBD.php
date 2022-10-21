4a1e56053d884c63186114e04355d46ee3a85b29
tw1ZTER <al3x_18_139@hotmail.com>
2022-10-21 02:53:40 -0300

    asdasd

--- php/conexionBD.php
+++ php/conexionBD.php
@@ -5,9 +5,6 @@ $dbName = 'mariadbspeedprog';
 $dbUser = 'root';
 $dbPassword = '';
 $conexion = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die(mysql_error());
-
-sadasdasdasdasdasdasd
-
 */
 
 
