f997e5112e3dd1a03c06b366a9b6dec2bb9c6ccb
tw1ZTER <al3x_18_139@hotmail.com>
2022-11-14 21:35:16 -0300

    proceso de notificaciones

--- php/conexionBD.php
+++ php/conexionBD.php
@@ -1,15 +1,15 @@
 <?php 
 
-
+/*
 //Conexion de Byron
 $dbHost = 'localhost';
 $dbName = 'speedprogasesorias';
 $dbUser = 'root';
 $dbPassword = '';
 $conexion = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die(mysql_error());
+*/
 
 
-/*
 
 //Conexion de Alex
 $dbHost = 'localhost:3306';
@@ -19,7 +19,7 @@ $dbPassword = 'root';
 $conexion = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die(mysql_error());
 
 
-
+/*
 //Conexion de pagina Web
 $dbHost = 'localhost';
 $dbName = 'speedpro_speedprogasesorias';
